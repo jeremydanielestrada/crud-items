8aae51b983acdfd6441004bb0b2e92b35f77a9a0
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-06-14 09:39:46 +0800

    added items model'

---
+++ app/Models/Items.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Items extends Model
+{
+      //The table associated with the model
+
+    protected $table = 'items';
+
+    //The primary key associated with the table
+    protected $primaryKey = 'item_id';
+
+
+
+    //The attributes that are mass assignable
+    protected $fillable = [
+        'item_name',
+        'image_path',
+        'description',
+        'user_id',
+    ] ;
+}
--- database/migrations/2025_06_14_011842_create_items_table.php
+++ database/migrations/2025_06_14_011842_create_items_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
 
            Schema::table('items', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
-        
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
