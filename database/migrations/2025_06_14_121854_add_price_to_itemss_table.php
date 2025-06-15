9318cea6a14658bb8dc33e5a2e9d420f4853f897
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-06-15 11:45:35 +0800

    Added data structure

--- database/migrations/2025_06_14_121854_add_price_to_itemss_table.php
+++ database/migrations/2025_06_14_121854_add_price_to_itemss_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('items', function (Blueprint $table) {
-            //
+            $table->float('price')->nullable();
         });
     }
 
